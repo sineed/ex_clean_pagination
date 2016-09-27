2f64c5e8cf33d7b952af68ab574d22f2c40f3125
Denis Tataurov <sineedus@mail.ru>
2016-09-27 18:46:16 +0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -10,28 +10,9 @@ defmodule ExCleanPagination.Mixfile do
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps
-    ]
-  end
-
- defp description do
-    """
-    API pagination the way RFC7233 intended it
-    """
-  end
-
-  def package do
-    [
-      maintainers: ["Denis Tataurov"],
-      files: [
-        "lib/ex_clean_pagination.ex",
-        "lib/ex_clean_pagination",
-        "mix.exs",
-        "README.md",
-        "LICENSE*"
-      ],
-      licenses: ["MIT"],
-      links: %{github: "https://github.com/sineed/ex_clean_pagination"}
+      deps: deps,
+      description: description,
+      package: package
     ]
   end
 
@@ -39,7 +20,30 @@ defmodule ExCleanPagination.Mixfile do
     [applications: [:logger]]
   end
 
+  defp description do
+    """
+    API pagination the way RFC7233 intended it
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Denis Tataurov"],
+      files: [
+        "lib/ex_clean_pagination.ex",
+        "lib/ex_clean_pagination",
+        "mix.exs",
+        "README.md"
+      ],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/sineed/ex_clean_pagination"}
+    ]
+  end
+
   defp deps do
-    [{:plug, "~> 1.2.0"}]
+    [
+      {:plug, "~> 1.2.0"},
+      {:ex_doc, "~> 0.13", only: :dev}
+    ]
   end
 end
